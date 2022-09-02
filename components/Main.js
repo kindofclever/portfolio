734d22781c75117b4c0e497a0c82fb63d0ed816e
Sandra Gufler <97165184+kindofclever@users.noreply.github.com>
2022-09-02 13:56:56 +0200

    landing page done

---
+++ components/Main.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { IoLogoLinkedin, IoMdMail } from 'react-icons/io';
+import { ImGithub } from 'react-icons/im';
+import { SiCodewars } from 'react-icons/si';
+
+const Main = () => {
+  return (
+    <div className='w-full h-screen text-center xs:mt-10'>
+        <div className='max-w-[1240px] w-full bottom-0 flex justify-center items-centers lg:mt-[10%]'>
+            <div>
+                <p className='uppercase text-sm tracking-widest text-[#0077B6]'>Nice that you are visiting me</p>
+                <h1 className='text-white'>Hej, my name is <span className='text-[#ffbf00]'>Sandra</span></h1>
+                <h1 className='text-[#03055e]'>I'm a fullstack web developer</h1>
+                <p className='pt-4 pb-0 max-w-[70%] m-auto'>
+                    At the beginning of this year I got the great news that I was chosen from more than a 1000 applicants to 
+                    attend the <span>School of Applied Thechnology JavaScript Bootcamp</span>. I got familiar with <span className=''>TDD, agile working methods,</span>
+                    <span className=''>mob programming</span> and the tools modern fullstack developers work with. I know the principles of <span className=''>Clean Code </span> 
+                    and I understand the importance of <span className=''>CI/CD</span>.
+                </p>
+                <div className='flex items-center justify-evenly max-w-[350px] m-auto pt-5 sm:max-w[300px] md:pt-5'>
+                    <div className='bg-[#0077B6] text-[#ff9e00] p-2 m-1 rounded-full shadow-xl cursor-pointer hover:scale-[125%] ease-in duration-200'>
+                        <IoLogoLinkedin size={30}/>
+                    </div>
+                    <div className='bg-[#0077B6] text-[#FF9E00] p-2 m-1 rounded-full shadow-xl cursor-pointer hover:scale-[125%] ease-in duration-200'>
+                        <ImGithub size={30} />
+                    </div>
+                    <div className='bg-[#0077B6] text-[#FF9E00] p-2 m-1 rounded-full shadow-xl cursor-pointer hover:scale-[125%] ease-in duration-200'>
+                        <IoMdMail size={30} />
+                    </div>
+                    <div className='bg-[#0077B6] text-[#FF9E00] p-2 m-1 rounded-full shadow-xl cursor-pointer hover:scale-[125%] ease-in duration-200'>
+                        <SiCodewars size={30} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Main;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,45 +1,120 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import {RiMenu4Fill} from 'react-icons/ri'
+import { RiMenu4Fill } from 'react-icons/ri';
+import { VscChromeClose } from 'react-icons/vsc';
+import { IoLogoLinkedin, IoMdMail } from 'react-icons/io';
+import { ImGithub } from 'react-icons/im';
+import { SiCodewars } from 'react-icons/si';
+
 
 const Navbar = () => {
-  return (
-    <div className='fixed w-full h-20 shadow-xl z-[100]'>
-        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-            <Image 
-                src='/../public/assets/logo/logo2.png' 
-                alt='logo' 
-                width='90' 
-                height='90' /> 
-            <div>
-                <ul className='hidden md:flex'>
-                    <Link href='/'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
-                    </Link>
-                    <Link href='/'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>About me</li>
-                    </Link>
-                    <Link href='/'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>Skills I have to offer</li>
-                    </Link>
-                    <Link href='/'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
-                    </Link>
-                    <Link href='/'>
-                        <li className='ml-10 mr-10 text-sm uppercase hover:border-b'>Get in touch</li>
-                    </Link>
-                </ul>
-                <div className='md:hidden mr-10'>
-                    <RiMenu4Fill size={35} className='text-[#ffbf00]' />
+
+    const [navbar, setNavbar] = useState(false);
+
+    const handleNavClick = () => {
+        setNavbar(!navbar);
+    };
+
+    return (
+        <header className='bg-[#0077B6] sticky w-full h-full z-[100] shadow-xl '>
+            <div className='flex justify-between items-center w-full h-full mb-5 p-2 2xl:px-16'>
+                <Image 
+                    className='mt-2'
+                    src='/../public/assets/logo/logo2.png' 
+                    alt='logo' 
+                    width='100' 
+                    height='100' /> 
+                <div>
+                    <ul className='hidden md:flex'>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>About me</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Skills I have to offer</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Blog</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 mr-10 text-sm uppercase hover:border-b'>Get in touch</li>
+                        </Link>
+                    </ul>
+                    <div onClick={handleNavClick} className='md:hidden mr-6'>
+                        <RiMenu4Fill size={35} className='text-[#ffbf00]' />
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className='fixed left-0 top-0 w-full h-screen bg-[#0077B6]'>
+            <div className={navbar ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-[#0076b67b]' : ''}>
+                <div className={
+                    navbar 
+                        ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#00B4D8] p-10 ease-in duration-200' 
+                        : 'fixed left-[-150%] top-0 p-10 ease-out duration-200'}>
+                    <div>
+                        <div className='flex w-full items-center justify-between'>
+                        <Image 
+                            className='mt-5'
+                            src='/../public/assets/logo/logo2.png' 
+                            alt='logo' 
+                            width='80' 
+                            height='70' />
+                            <div onClick={handleNavClick} className='hover:text-[#ffbf00] p-2 rounded-full shadow-xl cursor-pointer'>
+                                <VscChromeClose size={30} />
+                            </div>
+                        </div>
+                        <div className='border-b border-[#ffbf00] my-5'>
+                            <p className='w-[90%] md:w-[95%] py-3'>Let's check if I'm a good fit for your company!</p>
+                        </div>
+                    </div>
+                    <div>
+                        <ul className='uppercase py-5 text-white text-sm flex-col'>
+                            <Link href='/'>
+                                <li className='py-2'>Home</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='py-2'>About me</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='py-2'>Skills I have to offer</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='py-2'>Projects</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='py-2'>Blog</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='py-2'>Get in touch</li>
+                            </Link>
+                        </ul>
+                        <div className='py-'>
+                            <p className='uppercase text-xs mt-10 tracking-widest text-[#ffbf00] font-bold'>Let's connect!</p>
+                            <div className='flex items-center justify-between my-4 w-full sm:w-[90%]'>
+                                <div className='p-2 m-1 rounded-full shadow-xl cursor-pointer hover:scale-105 ease-in duration-200'>
+                                    <IoLogoLinkedin size={30}/>
+                                </div>
+                                <div className='p-2 m-1 rounded-full shadow-xl cursor-pointer hover:scale-105 ease-in duration-200'>
+                                    <ImGithub size={30} />
+                                </div>
+                                <div className='p-2 m-1 rounded-full shadow-xl cursor-pointer hover:scale-105 ease-in duration-200'>
+                                    <IoMdMail size={30} />
+                                </div>
+                                <div className='p-2 m-1 rounded-full shadow-xl cursor-pointer hover:scale-105 ease-in duration-200'>
+                                    <SiCodewars size={30} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
 
-        </div>
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
