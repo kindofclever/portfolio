487b487efd04796bef5a66486705394e07e035b4
Sandra Gufler <97165184+kindofclever@users.noreply.github.com>
2022-09-02 22:27:53 +0200

    fixed skills

--- components/Skills.js
+++ components/Skills.js
@@ -1,123 +1,100 @@
-import Image from 'next/image'
-import React from 'react'
+import React from 'react';
+import { AiFillHtml5 } from 'react-icons/ai';
+import { IoLogoCss3, IoLogoNodejs } from 'react-icons/io';
+import { DiJavascript } from 'react-icons/di';
+import { SiNextdotjs, SiReact, SiGithub, SiAmazonaws } from 'react-icons/si';
 
 const Skills = () => {
   return (
-    <div className ='ml-5 sm:ml-1 w-full lg:h-screen p-2'>
+    <div className ='ml-5 w-full lg:h-screen p-2'>
         <div className ='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
             <p className='text-[#0077B6] uppercase text-xl tracking-widest'>Skills</p>
-            <h2 className='text-[#ffbf00] font-bold'>What can I do for you?</h2>
-            <div className=' mt-5 sm:mr-5 md:mr-5 grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <div className='bg-white p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-30'>
+            <h2 className='text-[#ffbf00] font-bold mt-3'>What can I do for you?</h2>
+            <div className='mr-5 mt-8 grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+                <div className='bg-white p-6 shadow-xl rounded-xl text-white hover:scale-105 ease-in duration-30'>
                     <div className='grid grid-cols-2 justify-center gap-4 items-center'>
                         <div className='m-auto'>
-                            <Image alt='company icon' src='/../public/assets/skills/html.png' width='64' height='64'/>
-                        </div>
+                            <AiFillHtml5 className='text-[#FF6D00]' size={60} />
+                        </div> 
                         <div className='flex flex-col justify-center items-center'>
-                            <h3>HTML</h3>
+                            <h3 className='text-[#023E8A]'>HTML</h3>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className=' mt-5 grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <div className='bg-white p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-30'>
+                <div className='bg-white p-6 shadow-xl rounded-xl text-white hover:scale-105 ease-in duration-30'>
                     <div className='grid grid-cols-2 justify-center gap-4 items-center'>
                         <div className='m-auto'>
-                            <Image alt='company icon' src='/../public/assets/skills/css.png' width='64' height='64'/>
+                            <IoLogoCss3 className='text-[#FF6D00]' size={60} />
                         </div>
                         <div className='flex flex-col justify-center items-center'>
-                            <h3>HTML</h3>
+                            <h3 className='text-[#023E8A]'>CSS</h3>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className=' mt-5 grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <div className='bg-white p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-30'>
+                <div className='bg-white p-6 shadow-xl rounded-xl text-white hover:scale-105 ease-in duration-30'>
                     <div className='grid grid-cols-2 justify-center gap-4 items-center'>
                         <div className='m-auto'>
-                            <Image alt='company icon' src='/../public/assets/skills/javascript.png' width='64' height='64'/>
+                            <DiJavascript className='text-[#FF6D00]' size={60} />
                         </div>
                         <div className='flex flex-col justify-center items-center'>
-                            <h3>HTML</h3>
+                            <h3 className='text-[#023E8A]'>JavaScript</h3>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className=' mt-5 grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <div className='bg-white p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-30'>
+                <div className='bg-white p-6 shadow-xl rounded-xl text-white hover:scale-105 ease-in duration-30'>
                     <div className='grid grid-cols-2 justify-center gap-4 items-center'>
                         <div className='m-auto'>
-                            <Image alt='company icon' src='/../public/assets/skills/node.png' width='64' height='64'/>
+                            <IoLogoNodejs className='text-[#FF6D00]' size={60} />
                         </div>
                         <div className='flex flex-col justify-center items-center'>
-                            <h3>HTML</h3>
+                            <h3 className='text-[#023E8A]'>node.js</h3>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className=' mt-5 grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <div className='bg-white p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-30'>
+                <div className='bg-white p-6 shadow-xl rounded-xl text-white hover:scale-105 ease-in duration-30'>
                     <div className='grid grid-cols-2 justify-center gap-4 items-center'>
                         <div className='m-auto'>
-                            <Image alt='company icon' src='/../public/assets/skills/react.png' width='64' height='64'/>
+                            <SiReact className='text-[#FF6D00]' size={60} />
                         </div>
                         <div className='flex flex-col justify-center items-center'>
-                            <h3>HTML</h3>
+                            <h3 className='text-[#023E8A]'>react.js</h3>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className=' mt-5 grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <div className='bg-white p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-30'>
+                <div className='bg-white p-6 shadow-xl rounded-xl text-white hover:scale-105 ease-in duration-30'>
                     <div className='grid grid-cols-2 justify-center gap-4 items-center'>
                         <div className='m-auto'>
-                            <Image alt='company icon' src='/../public/assets/skills/next.png' width='64' height='64'/>
+                            <SiNextdotjs className='text-[#FF6D00]' size={60} />
                         </div>
                         <div className='flex flex-col justify-center items-center'>
-                            <h3>HTML</h3>
+                            <h3 className='text-[#023E8A]'>next.js</h3>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className=' mt-5 grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <div className='bg-white p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-30'>
+                <div className='bg-white p-6 shadow-xl rounded-xl text-white hover:scale-105 ease-in duration-30'>
                     <div className='grid grid-cols-2 justify-center gap-4 items-center'>
                         <div className='m-auto'>
-                            <Image alt='company icon' src='/../public/assets/skills/mongo.png' width='64' height='64'/>
+                            <SiGithub className='text-[#FF6D00]' size={60} />
                         </div>
                         <div className='flex flex-col justify-center items-center'>
-                            <h3>HTML</h3>
+                            <h3 className='text-[#023E8A]'>Github</h3>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className=' mt-5 grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <div className='bg-white p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-30'>
+                <div className='bg-white p-6 shadow-xl rounded-xl text-white hover:scale-105 ease-in duration-30'>
                     <div className='grid grid-cols-2 justify-center gap-4 items-center'>
                         <div className='m-auto'>
-                            <Image alt='company icon' src='/../public/assets/skills/github.png' width='64' height='64'/>
+                            <SiAmazonaws className='text-[#FF6D00]' size={60} />
                         </div>
                         <div className='flex flex-col justify-center items-center'>
-                            <h3>HTML</h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className=' mt-5 grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <div className='bg-white p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-30'>
-                    <div className='grid grid-cols-2 justify-center gap-4 items-center'>
-                        <div className='m-auto'>
-                            <Image alt='company icon' src='/../public/assets/skills/aws.png' width='64' height='64'/>
-                        </div>
-                        <div className='flex flex-col justify-center items-center'>
-                            <h3>HTML</h3>
+                            <h3 className='text-[#023E8A]'>AWS</h3>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
