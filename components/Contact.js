9e1ee6eea02b64a94fb515e5c00925d0c70181e0
Sandra Gufler <97165184+kindofclever@users.noreply.github.com>
2022-09-07 13:23:08 +0200

    same as before

--- components/Contact.js
+++ components/Contact.js
@@ -17,7 +17,7 @@ const Contact = () => {
             if (!field.name) return;
             formData[field.name] = field.value;
         });
-        fetch('/api/mail', {
+        await fetch('/api/mail', {
             method: 'post',
             body: JSON.stringify(formData),
         })
