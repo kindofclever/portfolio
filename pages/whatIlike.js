42a28a47910155dda55374b7307d81c060aeee29
Sandra Gufler <97165184+kindofclever@users.noreply.github.com>
2022-10-08 17:13:33 +0200

    fixed back button ball

--- pages/whatIlike.js
+++ pages/whatIlike.js
@@ -7,23 +7,25 @@ const whatIlike = () => {
   return (
     <>
       <div className="w-screen h-screen flex justify-center items-center">
-        <div className="flex justify-center items-center">
-          <h3 className="text-white text-3xl">
-            This page needs to be implemented...
-          </h3>
+        <div>
+          <div className="flex justify-center items-center">
+            <h3 className="text-white text-3xl">
+              This page needs to be implemented...
+            </h3>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center">
-        <Link href="/#about">
-          <motion.div
-            className="text-[#0077B6] hover:text-[#ff4369] p-2 m-1 cursor-pointer"
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.9 }}
-            transition={{ type: 'spring', stiffness: 400, damping: 17 }}
-          >
-            <FaAngleDoubleLeft size={35} />
-          </motion.div>
-        </Link>
+        <div className="flex justify-center">
+          <Link href="/#about">
+            <motion.div
+              className="text-[#0077B6] hover:text-[#ff4369] p-2 m- cursor-pointer"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+            >
+              <FaAngleDoubleLeft size={35} />
+            </motion.div>
+          </Link>
+        </div>
       </div>
     </>
   );
