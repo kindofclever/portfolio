0da4a9e4ee290d2654f9e2d75d9d303990314d6f
Sandra Gufler <97165184+kindofclever@users.noreply.github.com>
2022-10-11 11:21:45 +0200

    fixed mobile view

--- pages/whatIlike.js
+++ pages/whatIlike.js
@@ -37,30 +37,36 @@ const whatIlike = ({ aboutMe }) => {
     <div className="w-screen h-screen flex flex-col justify-start items-center">
       <div className=" m-5 sm:mr-5  md:mr-5 md:h-screen p-2 flex flex-col items-start sticky w-full lg:h-screen">
         <div className="flex flex-col">
-          <div className="grid grid-cols-2 mb-[1%]">
-            <div className="flex flex-col justify-start">
-              <p className="uppercase text-xl tracking-widest">About me</p>
-              <h2 className="text-[#ffec43] font-bold mt-3">{title}</h2>
+          <div className="grid md:grid-cols-2 mb-[1%]">
+            <div className="flex ml-5 flex-col justify-start">
+              <p className="uppercase text-3xl md:text-xl tracking-widest">
+                About me
+              </p>
+              <h2 className="text-[#ffec43] text-4xl font-bold mt-3">
+                {title}
+              </h2>
             </div>
-            <Slider aboutMe={aboutMe} />
+            <div className="m-5 md:ml-[-1%] rounded-xl">
+              <Slider aboutMe={aboutMe} />
+            </div>
           </div>
-          <div className="pb-[3%] grid grid-cols-2 ml-5 mr-9">
-            <h4 className="uppercase text-white text-2xl tracking-widest mb-1">
+          <div className=" pb-[5%] md:pb-[3%] grid md:grid-cols-2 ml-5 mr-9">
+            <h4 className="uppercase text-white text-3xl md:text-2xl pb-[3%] md:pb-[0%] tracking-widest mb-1">
               {subtitle1}
             </h4>
-            <p>{paragraph1}</p>
+            <p className="text-xl md:text-base">{paragraph1}</p>
           </div>
-          <div className="pb-[3%] grid grid-cols-2 ml-5 mr-9">
-            <h4 className="uppercase text-white text-2xl tracking-widest mb-1">
+          <div className="pb-[3%] grid md:grid-cols-2 ml-5 mr-9">
+            <h4 className="uppercase text-white text-3xl md:text-2xl pb-[3%] md:pb-[0%] tracking-widest mb-1">
               {subtitle2}
             </h4>
-            <p>{paragraph2}</p>
+            <p className="text-xl md:text-base">{paragraph2}</p>
           </div>
-          <div className="mb-5 grid grid-cols-2 ml-5 mr-9">
-            <h4 className="uppercase text-white text-2xl tracking-widest mb-1">
+          <div className="mb-5 grid md:grid-cols-2 ml-5 mr-9">
+            <h4 className="uppercase text-white text-3xl md:text-2xl pb-[3%] md:pb-[0%] tracking-widest mb-1">
               {subtitle3}
             </h4>
-            <p>{paragraph3}</p>
+            <p className="text-xl md:text-base">{paragraph3}</p>
           </div>
           <div className="flex justify-center">
             <Link href="/">
