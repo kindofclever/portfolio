575bbd0c6e7c22d769fbb43dcb4e1a69d3c8d185
Sandra Gufler <97165184+kindofclever@users.noreply.github.com>
2022-10-12 16:49:20 +0200

    we will see

--- components/About.js
+++ components/About.js
@@ -11,14 +11,14 @@ const About = () => {
       className="mr-0 ml-5 pb-[95%] md:pb-[5%] md:m-5"
     >
       <div className="md:mr-5 md:h-screen flex flex-col   items-start sticky w-full lg:h-screen pt-[7%]">
-        <div className="flex flex-col justify-start mb-[3%] px-0 md:px-2  ">
+        <div className="flex flex-col justify-start px-0 md:px-2  ">
           <p className=" uppercase text-xl tracking-widest">About me</p>
           <h2 className="text-[#ffec43] font-bold mt-3">Who am I?</h2>
           <p className="text-white">
             Click on one of the objects to find out more about me!
           </p>
         </div>
-        <div className="w-[80%] mx-auto shadow-xl h-[95%]">
+        <div className="w-[90%] md:w-[90%] mt-[20%] md:mt-2 mx-auto shadow-xl h-[60%] md:h-[90%]">
           <Canvas className="" id="three-canvas-container" shadows>
             <Suspense fallback={<></>}>
               <Three />
--- components/Skills.js
+++ components/Skills.js
@@ -19,238 +19,146 @@ import {
 } from 'react-icons/si';
 import { motion as m } from 'framer-motion';
 import { useState, useRef, useEffect } from 'react';
+import Marquee from 'react-fast-marquee';
 
 const Skills = () => {
-  const [width, setWidth] = useState(0);
-  const carousel = useRef();
-
-  useEffect(() => {
-    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-    console.log(carousel.current);
-  }, []);
-
   return (
     <m.div
       exit={{ opacity: 0 }}
       id="skills"
-      className=" sticky w-full lg:h-screen pt-[15%] m-5 "
+      className=" sticky  lg:h-screen pt-[15%] pb-[95%] md:pb-[15%]  m-5 "
     >
-      <div className=" px-0 md:px-2 flex flex-col justify-center h-full">
+      <div className=" px-0 md:px-2 flex flex-col justify-center">
         <p className="uppercase text-xl tracking-widest">Skills</p>
         <h2 className="text-[#ffec43] font-bold mt-3">
           What can I do for you?
         </h2>
-        <p className="text-white">Slide through some of my skills!</p>
-        <m.div
-          ref={carousel}
-          className="cursor-grab overflow-hidden mr-5 md:mr-10"
-        >
-          <m.div
-            drag="x"
-            dragConstraints={{ right: 0, left: -width }}
-            className="flex"
-          >
-            <m.div className="min-h-[40rem] min-w-[18rem] md:min-w-[24rem]  p-[40px]  flex flex-col">
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5 mb-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <AiFillHtml5 className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">HTML</h3>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <IoLogoCss3 className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">CSS</h3>
-                  </div>
-                </div>
-              </div>
-            </m.div>
-
-            <m.div className="min-h-[40rem] min-w-[18rem] md:min-w-[22rem]  p-[40px]  flex flex-col">
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5 mb-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <DiJavascript className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">JavaScript</h3>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiTypescript className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">Typescript</h3>
-                  </div>
-                </div>
-              </div>
-            </m.div>
-            <m.div className="min-h-[40rem] min-w-[18rem] md:min-w-[22rem]  p-[40px]  flex flex-col">
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5 mb-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiReact className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">React.js</h3>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiReact className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center ">
-                    <h3 className=" text-[#03045E] ">React native</h3>
-                  </div>
-                </div>
-              </div>
-            </m.div>
-            <m.div className="min-h-[40rem] min-w-[18rem] md:min-w-[24rem]  p-[40px]  flex flex-col">
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5 mb-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiNextdotjs className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">Next.js</h3>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiFramer className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">Framer Motion</h3>
-                  </div>
-                </div>
-              </div>
-            </m.div>
-            <m.div className="min-h-[40rem] min-w-[18rem] md:min-w-[22rem]  p-[40px]  flex flex-col">
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5 mb-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <IoLogoNodejs className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">Node.js</h3>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiExpress className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">Express</h3>
-                  </div>
-                </div>
-              </div>
-            </m.div>
-            <m.div className="min-h-[40rem] min-w-[18rem] md:min-w-[22rem]  p-[40px]  flex flex-col">
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5 mb-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiEslint className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">ESLint</h3>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <ImGit className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">Git</h3>
-                  </div>
-                </div>
-              </div>
-            </m.div>
-            <m.div className="min-h-[40rem] min-w-[18rem] md:min-w-[22rem]  p-[40px]  flex flex-col">
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5 mb-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiGithub className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">GitHub</h3>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiMongodb className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">MongoDB</h3>
-                  </div>
-                </div>
-              </div>
-            </m.div>
-            <m.div className="min-h-[40rem] min-w-[18rem] md:min-w-[22rem]  p-[40px]  flex flex-col">
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5 mb-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiPostgresql className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">PostgreSQL</h3>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiAmazonaws className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">AWS</h3>
-                  </div>
-                </div>
-              </div>
-            </m.div>
-            <m.div className="min-h-[40rem] min-w-[18rem] md:min-w-[22rem]  p-[40px]  flex flex-col">
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5 mb-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiFigma className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">Figma</h3>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-white p-6 shadow-xl rounded-xl px-5">
-                <div className="grid grid-cols-2 justify-center gap-4 items-center">
-                  <div className="m-auto">
-                    <SiTrello className="text-[#03045E]" size={60} />
-                  </div>
-                  <div className="flex flex-col justify-center items-center">
-                    <h3 className="text-[#03045E]">Trello</h3>
-                  </div>
-                </div>
-              </div>
-            </m.div>
+        <p className="text-white">
+          Hover over the slider to pause the animation!
+        </p>
+        <div>
+          <m.div className="flex mt-[20%] md:mt-7">
+            <Marquee
+              speed="120"
+              gradientWidth="0"
+              pauseOnHover
+              className="-rotate-6 bg-[#ffec43] p-5"
+            >
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <AiFillHtml5 className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">HTML</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <IoLogoCss3 className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">CSS</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <DiJavascript className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">JavaScript</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <SiTypescript className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">Typescript</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <SiReact className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">React.js</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <SiNextdotjs className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">Next.js</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <SiFramer className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">Framer Motion</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <IoLogoNodejs className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">Node.js</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <SiExpress className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">Express</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <SiEslint className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">ESLint</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <ImGit className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">Git</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <SiGithub className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">GitHub</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <SiMongodb className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">MongoDB</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <SiPostgresql className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">PostgreSQL</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <SiFigma className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">Figma</h3>
+              </m.div>
+              <m.div
+                whileHover={{ scale: 1.2 }}
+                className="flex flex-col justify-center items-center bg-white m-5 w-[200px] rounded-xl p-5 h-[200px] "
+              >
+                <SiTrello className="text-[#03045E]" size={80} />
+                <h3 className="text-[#03045E]">Trello</h3>
+              </m.div>
+            </Marquee>
           </m.div>
-        </m.div>
+        </div>
       </div>
     </m.div>
   );
